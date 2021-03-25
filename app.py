c7bef41c01e0b1d3d8dd6cc5ff27e4013fc1a73c
nikitaknyazev <nikitaknyazev@yandex.ru>
2021-03-25 14:05:08 +0300

    4

--- app.py
+++ app.py
@@ -2,36 +2,60 @@
 import os, random
 import flask
 from flask import Flask, render_template, request, send_file
-import pandas as pd
-
-columns = ['id', 'name1', 'name2', 'name3', 'gender', 'age', 'blood', 'pulse']
-df = pd.DataFrame(columns = columns)
-
-df.loc[len(df)] = [len(df), 'Ivan', 'Ivanov', 'Ivanovich', 'M', str(random.randint(20, 80)), 'A(II) Rh+', random.randint(50, 120)]
-df.loc[len(df)] = [len(df), 'Petr', 'Petrov', 'Petrovich', 'M', str(random.randint(20, 80)), 'O(I) Rh-', random.randint(50, 120)]
-df.loc[len(df)] = [len(df), 'Mary', 'Petrova', 'Ivanovna', 'F', str(random.randint(20, 80)), 'AB(III) Rh-', random.randint(50, 120)]
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+#database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    gender = db.Column(db.String(3), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    blood = db.Column(db.String(10), nullable=False)
+    pulse = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<users {self.id}>"
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', data = df)
+    return render_template('index.html', data = Users.query.all())
+
+@app.route('/users', methods=['GET', 'POST'])
+def add_user():
+    try:
+        p = Users(name=request.form['name'], gender=request.form['gender'], age=request.form['age'], blood=request.form['blood'])
+        db.session.add(p)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        print('Ошибка добавления')
+    return render_template('users.html', users=Users.query.all())
+
+@app.route('/del_user', methods=['GET', 'POST'])
+def del_user():
+    var = int(request.form['id'])
+    try:
+        p = Users.query.filter_by(id=var).first()
+        db.session.delete(p)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        print('Ошибка удаления')
+    return render_template('del_user.html', users=Users.query.all())
 
 @app.route('/people', methods=['GET', 'POST'])
 def people():
-    var = int(request.args.get('my_var', None)[1])
-    people = df[df['id']==var]
-    name1 = people['name1'].values[0]
-    name2 = people['name2'].values[0]
-    name3 = people['name3'].values[0]
-    gender = people['gender'].values[0]
-    age = people['age'].values[0]
-    blood = people['blood'].values[0]
-    people['pulse'] = random.randint(50, 120)
-    pulse = int(people['pulse'].values[0])
-    return render_template('form2.html', data = (name1, name2, name3, gender, age, blood, pulse))
-    #return render_template('form.html')
+    var = int(request.args.get('my_var', None))
+    p = Users.query.filter_by(id=var).first()
+    p.pulse = random.randint(50, 120)
+    db.session.commit()
+    return render_template('form2.html', data=p)
 
 '''@app.route('/sms', methods=['GET', 'POST'])
 def sms():
